b1deb00e3212c811865b49ac3b08b7d88b7b96e9
赵一楠 <zhaoyinan.1024@bytedance.com>
2022-11-05 08:31:52 +0800

    feat: 修改变量名称,增进理解

--- src/xhr-hook.js
+++ src/xhr-hook.js
@@ -1,128 +1,138 @@
-/*
- * author: wendux
- * email: 824783146@qq.com
- * source code: https://github.com/wendux/Ajax-hook
- */
+export const REAL_XHR_KEY = '__xhr';
+export const eventnames = {
+	load: 'load',
+	loadend: 'loadend',
+	timeout: 'timeout',
+	error: 'error',
+	readystatechange: 'readystatechange',
+	abort: 'abort'
+};
 
-// Save original XMLHttpRequest as _rxhr
-var realXhr = "__xhr"
+export function unHook(iframeWindow) {
+	const win = iframeWindow || window;
 
-export var events = ['load', 'loadend', 'timeout', 'error', 'readystatechange', 'abort'];
+	// Restore XHR if needed, and clear up
+	if (win[REAL_XHR_KEY]) {
+		win.XMLHttpRequest = win[REAL_XHR_KEY];
+	}
+	win[REAL_XHR_KEY] = undefined;
+}
+
+export function hook(hookObj, iframeWindow) {
+	const win = iframeWindow || window;
+
+	// Keep original XHR and replace with new function
+	win[REAL_XHR_KEY] = win[REAL_XHR_KEY] || win.XMLHttpRequest;
+	win.XMLHttpRequest = function () {
+		// Hook into XMLHttpRequest instead of its prototype, for some attributes are on itself.
+		const realXhr = new win[REAL_XHR_KEY]();
+
+		// Generate all callbacks(eg. onload) are enumerable (not undefined).
+		for (let eventname of Object.values(eventnames)) {
+			const key = `on${eventname}`;
+			if (realXhr[key] === undefined) {
+				realXhr[key] = null;
+			}
+		}
+
+		// hook realXhr 对象上的所有属性和方法，如 hookObj 传入了 hookHandler ，则会优先执行该 hookHanlder
+		for (let attr in realXhr) {
+			let type = '';
+			// May cause exception on some browser
+			try {
+				type = typeof realXhr[attr];
+			} catch (e) {}
+
+			if (type === 'function') {
+				// hook methods such as `open`, `send` ...
+				this[attr] = hookFunction(attr, hookObj);
+			} else {
+				// hook other properties and event handler properties such as: `onload`, `onreadystatechange` ...
+				Object.defineProperty(this, attr, {
+					get: getterFactory(attr, hookObj),
+					set: setterFactory(attr, hookObj),
+					enumerable: true
+				});
+			}
+			const that = this;
+			realXhr.getProxy = function () {
+				return that;
+			};
+			this.realXhr = realXhr;
+			Object.assign(win.XMLHttpRequest, {
+				UNSENT: 0,
+				OPENED: 1,
+				HEADERS_RECEIVED: 2,
+				LOADING: 3,
+				DONE: 4
+			});
+
+			// Return the real XMLHttpRequest
+			return win[REAL_XHR_KEY];
+		}
+	};
+
+	// Hook methods of xhr.
+	function hookFunction(funKey, hookObj) {
+		return function () {
+			var args = [].slice.call(arguments);
+			const hookFun = hookObj[funKey];
+			const realFun = this.realXhr[funKey];
+			if (hookFun) {
+				var ret = hookFun.call(this, args, this.realXhr);
+				// If the hookObj return value exists, return it directly,
+				// otherwise call the function of xhr.
+				if (ret) return ret;
+			}
+			return realFun.apply(this.realXhr, args);
+		};
+	}
+
+	// Generate getter for attributes of xhr
+	function getterFactory(attr, hookObj) {
+		return function () {
+			var v = this.hasOwnProperty(attr + '_') ? this[attr + '_'] : this.realXhr[attr];
+			var attrGetterHook = (hookObj[attr] || {})['getter'];
+			return (attrGetterHook && attrGetterHook(v, this)) || v;
+		};
+	}
+
+	// Generate setter for attributes of xhr; by this we have an opportunity
+	// to hookAjax event callbacks （eg: `onload`） of xhr;
+	function setterFactory(attr, hookObj) {
+		return function (v) {
+			var realXhr = this.realXhr;
+			var that = this;
+			var hook = proxy[attr];
+
+			// hook event handlers such as `onload`、`onreadystatechange`...
+			if (attr.substring(0, 2) === 'on') {
+				that[attr + '_'] = v;
+				realXhr[attr] = function (e) {
+					e = configEvent(e, that);
+					var ret = proxy[attr] && proxy[attr].call(that, realXhr, e);
+					ret || v.call(that, e);
+				};
+			} else {
+				//If the attribute isn't writable, generate proxy attribute
+				var attrSetterHook = (hook || {})['setter'];
+				v = (attrSetterHook && attrSetterHook(v, that)) || v;
+				this[attr + '_'] = v;
+				try {
+					// Not all attributes of xhr are writable(setter may undefined).
+					realXhr[attr] = v;
+				} catch (e) {}
+			}
+		};
+	}
+}
 
 export function configEvent(event, xhrProxy) {
-  var e = {};
-  for (var attr in event) e[attr] = event[attr];
-  // xhrProxy instead
-  e.target = e.currentTarget = xhrProxy
-  return e;
+	const e = {};
+	for (let attr in event) {
+		e[attr] = event[attr];
+	}
+	// xhrProxy instead
+	e.target = e.currentTarget = xhrProxy;
+	return e;
 }
-
-export function hook(proxy, win) {
-  win = win || window;
-  // Avoid double hookAjax
-  win[realXhr] = win[realXhr] || win.XMLHttpRequest
-
-  win.XMLHttpRequest = function () {
-
-    // We shouldn't hookAjax XMLHttpRequest.prototype because we can't
-    // guarantee that all attributes are on the prototype。
-    // Instead, hooking XMLHttpRequest instance can avoid this problem.
-
-    var xhr = new win[realXhr];
-
-
-    // Generate all callbacks(eg. onload) are enumerable (not undefined).
-    for (var i = 0; i < events.length; ++i) {
-      var key='on'+events[i];
-      if (xhr[key] === undefined) xhr[key] = null;
-    }
-
-    for (var attr in xhr) {
-      var type = "";
-      try {
-        type = typeof xhr[attr] // May cause exception on some browser
-      } catch (e) {
-      }
-      if (type === "function") {
-        // hookAjax methods of xhr, such as `open`、`send` ...
-        this[attr] = hookFunction(attr);
-      } else {
-        Object.defineProperty(this, attr, {
-          get: getterFactory(attr),
-          set: setterFactory(attr),
-          enumerable: true
-        })
-      }
-    }
-    var that = this;
-    xhr.getProxy = function () {
-      return that
-    }
-    this.xhr = xhr;
-  }
-
-  Object.assign(win.XMLHttpRequest, {UNSENT: 0, OPENED: 1, HEADERS_RECEIVED: 2, LOADING: 3, DONE: 4});
-
-  // Generate getter for attributes of xhr
-  function getterFactory(attr) {
-    return function () {
-      var v = this.hasOwnProperty(attr + "_") ? this[attr + "_"] : this.xhr[attr];
-      var attrGetterHook = (proxy[attr] || {})["getter"]
-      return attrGetterHook && attrGetterHook(v, this) || v
-    }
-  }
-
-  // Generate setter for attributes of xhr; by this we have an opportunity
-  // to hookAjax event callbacks （eg: `onload`） of xhr;
-  function setterFactory(attr) {
-    return function (v) {
-      var xhr = this.xhr;
-      var that = this;
-      var hook = proxy[attr];
-      // hookAjax  event callbacks such as `onload`、`onreadystatechange`...
-      if (attr.substring(0, 2) === 'on') {
-        that[attr + "_"] = v;
-        xhr[attr] = function (e) {
-          e = configEvent(e, that)
-          var ret = proxy[attr] && proxy[attr].call(that, xhr, e)
-          ret || v.call(that, e);
-        }
-      } else {
-        //If the attribute isn't writable, generate proxy attribute
-        var attrSetterHook = (hook || {})["setter"];
-        v = attrSetterHook && attrSetterHook(v, that) || v
-        this[attr + "_"] = v;
-        try {
-          // Not all attributes of xhr are writable(setter may undefined).
-          xhr[attr] = v;
-        } catch (e) {
-        }
-      }
-    }
-  }
-
-  // Hook methods of xhr.
-  function hookFunction(fun) {
-    return function () {
-      var args = [].slice.call(arguments)
-      if (proxy[fun]) {
-        var ret = proxy[fun].call(this, args, this.xhr)
-        // If the proxy return value exists, return it directly,
-        // otherwise call the function of xhr.
-        if (ret) return ret;
-      }
-      return this.xhr[fun].apply(this.xhr, args);
-    }
-  }
-
-  // Return the real XMLHttpRequest
-  return win[realXhr];
-}
-
-export function unHook(win) {
-  win = win || window
-  if (win[realXhr]) win.XMLHttpRequest = win[realXhr];
-  win[realXhr] = undefined;
-}
-
-
--- src/xhr-proxy.js
+++ src/xhr-proxy.js
@@ -1,247 +1,254 @@
-/*
- * author: wendux
- * email: 824783146@qq.com
- * source code: https://github.com/wendux/Ajax-hook
- */
+import { unHook, hook, REAL_XHR_KEY, configEvent, eventnames } from './xhr-hook';
 
-import {hook, unHook, configEvent, events} from "./xhr-hook";
-
-var eventLoad = events[0],
-  eventLoadEnd = events[1],
-  eventTimeout = events[2],
-  eventError = events[3],
-  eventReadyStateChange = events[4],
-  eventAbort = events[5];
-
-
-var prototype = 'prototype';
-
-
-export function proxy(proxy, win) {
-  win = win || window;
-  if (win['__xhr']) throw "Ajax is already hooked.";
-  return proxyAjax(proxy, win);
+export function unProxy(iframeWindow) {
+	unHook(iframeWindow);
 }
 
-export function unProxy(win) {
-  unHook(win)
+// iframe.contentWindow 传入。注意：只能拦截同源的iframe页面（不能跨域）
+export function proxy(proxyObj, iframeWindow) {
+	const win = iframeWindow || window;
+
+	if (win[REAL_XHR_KEY]) {
+		throw 'Ajax is already hooked.';
+	}
+	return proxyAjax(proxyObj, win);
+}
+
+function proxyAjax(proxyObj, win) {
+	const { onRequest, onResponse, onError } = proxyObj;
+
+	function handleResponse(realXhr, xhrProxy) {
+		var handler = new ResponseHandler(realXhr);
+		var resObj = {
+			response: xhrProxy.response || xhrProxy.responseText, //ie9
+			status: xhrProxy.status,
+			statusText: xhrProxy.statusText,
+			config: realXhr.config,
+			headers:
+				realXhr.resHeader ||
+				realXhr
+					.getAllResponseHeaders()
+					.split('\r\n')
+					.reduce(function (ob, str) {
+						if (str === '') return ob;
+						var m = str.split(':');
+						ob[m.shift()] = trim(m.join(':'));
+						return ob;
+					}, {})
+		};
+		if (!onResponse) {
+			return handler.next(resObj);
+		}
+		onResponse(resObj, handler);
+	}
+
+	function onerror(xhr, xhrProxy, error, errorname) {
+		var handler = new ErrorHandler(xhr);
+		error = { config: xhr.config, error: error, type: errorname };
+		if (onError) {
+			onError(error, handler);
+		} else {
+			handler.next(error);
+		}
+	}
+
+	function makeErrorCallback(errorname) {
+		return function (xhr, e) {
+			onerror(xhr, this, e, errorname);
+			return true;
+		};
+	}
+
+	function stateChangeCallback(realXhr, xhrProxy) {
+		if (realXhr.readyState === 4 && realXhr.status !== 0) {
+			handleResponse(realXhr, xhrProxy);
+		} else if (realXhr.readyState !== 4) {
+			triggerListener(realXhr, eventnames.readystatechange);
+		}
+		return true;
+	}
+
+	// hook 函数的第一个参数，是一个对象，其属性代表了需要对 XHR 拦截
+	return hook(
+		{
+			// onload/onloadend 是事件处理函数，会通过事件触发来调用，
+			// 因此返回 true ，防止调用 realXhr 上的方法
+			onload: function preventRealXhrMethdCall() {
+				return true;
+			},
+			onloadend: function preventRealXhrMethdCall() {
+				return true;
+			},
+			onerror: makeErrorCallback(eventnames.error),
+			ontimeout: makeErrorCallback(eventnames.timeout),
+			onabort: makeErrorCallback(eventnames.abort),
+			onreadystatechange: function (realXhr) {
+				return stateChangeCallback(realXhr, this);
+			},
+
+			open(args, realXhr) {
+				var _this = this;
+
+				// hook open 方法的调用，并将相关参数记录下来，保存到 realXhr.config 对象上
+				var config = (realXhr.config = { headers: {} });
+				config.method = args[0];
+				config.url = args[1];
+				config.async = args[2];
+				config.user = args[3];
+				config.password = args[4];
+				config.realXhr = realXhr;
+
+				// 兼容未监听 'onreadystatechange' 的情况
+				var handlerKey = 'on' + eventnames.readystatechange;
+				if (!realXhr[handlerKey]) {
+					realXhr[handlerKey] = function () {
+						return stateChangeCallback(realXhr, _this);
+					};
+				}
+
+				// TODO to dig deeper
+				// 如有请求拦截器，则在调用onRequest后再打开链接。因为onRequest最佳调用时机是在send前，
+				// 所以我们在send拦截函数中再手动调用open，因此返回true阻止xhr.open调用。
+				// 如没有请求拦截器，则不用阻断xhr.open调用
+				if (onRequest) return true;
+			},
+			send(args, realXhr) {
+				var config = realXhr.config;
+				config.withCredentials = realXhr.withCredentials;
+				config.body = args[0];
+
+				if (onRequest) {
+					// In 'onRequest', we may call XHR's event handler, such as `xhr.onload`.
+					// However, XHR's event handler may not be set until xhr.send is called in
+					// the user's code, so we use `setTimeout` to avoid this situation
+					var req = function () {
+						onRequest(config, new RequestHandler(realXhr));
+					};
+					config.async === false ? req() : setTimeout(req);
+					return true;
+				}
+			},
+			setRequestHeader: function (args, realXhr) {
+				realXhr.config.headers[args[0].toLowerCase()] = args[1];
+				if (onRequest) return true;
+			},
+			addEventListener: function (args, realXhr) {
+				var _this = this;
+				if (eventnames.includes(args[0])) {
+					var handler = args[1];
+					getEventTarget(realXhr).addEventListener(args[0], function (e) {
+						var event = configEvent(e, _this);
+						event.type = args[0];
+						event.isTrusted = true;
+						handler.call(_this, event);
+					});
+					return true;
+				}
+			},
+			getAllResponseHeaders: function (_, realXhr) {
+				var headers = realXhr.resHeader;
+				if (headers) {
+					var header = '';
+					for (var key in headers) {
+						header += key + ': ' + headers[key] + '\r\n';
+					}
+					return header;
+				}
+			},
+			getResponseHeader: function (args, realXhr) {
+				var headers = realXhr.resHeader;
+				if (headers) {
+					return headers[(args[0] || '').toLowerCase()];
+				}
+			}
+		},
+		win
+	);
 }
 
 function trim(str) {
-  return str.replace(/^\s+|\s+$/g, '');
+	return str.replace(/^\s+|\s+$/g, '');
 }
 
 function getEventTarget(xhr) {
-  return xhr.watcher || (xhr.watcher = document.createElement('a'));
+	return xhr.watcher || (xhr.watcher = document.createElement('a'));
 }
 
-function triggerListener(xhr, name) {
-  var xhrProxy = xhr.getProxy();
-  var callback = 'on' + name + '_';
-  var event = configEvent({type: name}, xhrProxy);
-  xhrProxy[callback] && xhrProxy[callback](event);
-  var evt;
-  if (typeof(Event) === 'function') {
-    evt = new Event(name, {bubbles: false});
-  } else {
-    // https://stackoverflow.com/questions/27176983/dispatchevent-not-working-in-ie11
-    evt = document.createEvent('Event');
-    evt.initEvent(name, false, true);
-  }
-  getEventTarget(xhr).dispatchEvent(evt);
+function triggerListener(realXhr, eventname) {
+	// 如用户有对事件处理函数赋值，则构造事件并调用该函数
+	var xhrProxy = realXhr.getProxy();
+	var callback = 'on' + eventname + '_';
+	var event = configEvent({ type: eventname }, xhrProxy);
+	xhrProxy[callback] && xhrProxy[callback](event);
+
+	// 构造 event 对象，创建一个 DOM 元素并派发该对象
+	var event;
+	if (typeof Event === 'function') {
+		event = new Event(eventname, { bubbles: false });
+	} else {
+		// https://stackoverflow.com/questions/27176983/dispatchevent-not-working-in-ie11
+		event = document.createEvent('Event');
+		event.initEvent(name, false, true);
+	}
+	getEventTarget(xhr).dispatchEvent(event);
 }
 
-
-function Handler(xhr) {
-  this.xhr = xhr;
-  this.xhrProxy = xhr.getProxy();
+function Handler(realXhr) {
+	this.realXhr = realXhr;
+	this.xhrProxy = realXhr.getProxy();
 }
 
-Handler[prototype] = Object.create({
-  resolve: function resolve(response) {
-    var xhrProxy = this.xhrProxy;
-    var xhr = this.xhr;
-    xhrProxy.readyState = 4;
-    xhr.resHeader = response.headers;
-    xhrProxy.response = xhrProxy.responseText = response.response;
-    xhrProxy.statusText = response.statusText;
-    xhrProxy.status = response.status;
-    triggerListener(xhr, eventReadyStateChange);
-    triggerListener(xhr, eventLoad);
-    triggerListener(xhr, eventLoadEnd);
-  },
-  reject: function reject(error) {
-    this.xhrProxy.status = 0;
-    triggerListener(this.xhr, error.type);
-    triggerListener(this.xhr, eventLoadEnd);
-  }
+Handler.prototype = Object.create({
+	resolve: function resolve(resObj) {
+		var realXhr = this.realXhr;
+		var xhrProxy = this.xhrProxy;
+
+		realXhr.resHeader = resObj.headers;
+
+		xhrProxy.readyState = 4;
+		xhrProxy.response = xhrProxy.responseText = resObj.response;
+		xhrProxy.statusText = resObj.statusText;
+		xhrProxy.status = resObj.status;
+
+		triggerListener(realXhr, eventnames.readystatechange);
+		triggerListener(realXhr, eventnames.load);
+		triggerListener(realXhr, eventnames.loadend);
+	},
+	reject: function reject(error) {
+		this.xhrProxy.status = 0;
+		var realXhr = this.realXhr;
+
+		triggerListener(realXhr, error.type);
+		triggerListener(realXhr, eventnames.loadend);
+	}
 });
 
+// 利用了组合继承的特性，实际上最终使用的是 sub 函数的实例
 function makeHandler(next) {
-  function sub(xhr) {
-    Handler.call(this, xhr);
-  }
+	function sub(realXhr) {
+		Handler.call(this, realXhr);
+	}
 
-  sub[prototype] = Object.create(Handler[prototype]);
-  sub[prototype].next = next
-  return sub;
+	sub.prototype = Object.create(Handler.prototype);
+	sub.prototype.next = next;
+	return sub;
 }
 
-var RequestHandler = makeHandler(function (rq) {
-  var xhr = this.xhr;
-  rq = rq || xhr.config;
-  xhr.withCredentials = rq.withCredentials;
-  xhr.open(rq.method, rq.url, rq.async !== false, rq.user, rq.password);
-  for (var key in rq.headers) {
-    xhr.setRequestHeader(key, rq.headers[key]);
-  }
-  xhr.send(rq.body);
+var RequestHandler = makeHandler(function (reqConfig) {
+	var realXhr = this.realXhr;
+	reqConfig = reqConfig || realXhr.config;
+	realXhr.withCredentials = reqConfig.withCredentials;
+	realXhr.open(reqConfig.method, reqConfig.url, reqConfig.async !== false, reqConfig.user, reqConfig.password);
+	for (var key in reqConfig.headers) {
+		realXhr.setRequestHeader(key, reqConfig.headers[key]);
+	}
+	realXhr.send(reqConfig.body);
 });
 
 var ResponseHandler = makeHandler(function (response) {
-  this.resolve(response);
+	this.resolve(response);
 });
 
 var ErrorHandler = makeHandler(function (error) {
-  this.reject(error);
+	this.reject(error);
 });
-
-function proxyAjax(proxy, win) {
-  var onRequest = proxy.onRequest,
-    onResponse = proxy.onResponse,
-    onError = proxy.onError;
-
-  function handleResponse(xhr, xhrProxy) {
-    var handler = new ResponseHandler(xhr);
-    var ret = {
-      response: xhrProxy.response || xhrProxy.responseText, //ie9
-      status: xhrProxy.status,
-      statusText: xhrProxy.statusText,
-      config: xhr.config,
-      headers: xhr.resHeader || xhr.getAllResponseHeaders().split('\r\n').reduce(function (ob, str) {
-        if (str === "") return ob;
-        var m = str.split(":");
-        ob[m.shift()] = trim(m.join(':'));
-        return ob;
-      }, {})
-    };
-    if (!onResponse) return handler.resolve(ret);
-    onResponse(ret, handler);
-  }
-
-  function onerror(xhr, xhrProxy, error, errorType) {
-    var handler = new ErrorHandler(xhr);
-    error = {config: xhr.config, error: error, type: errorType};
-    if (onError) {
-      onError(error, handler);
-    } else {
-      handler.next(error);
-    }
-  }
-
-  function preventXhrProxyCallback() {
-    return true;
-  }
-
-  function errorCallback(errorType) {
-    return function (xhr, e) {
-      onerror(xhr, this, e, errorType);
-      return true;
-    }
-  }
-
-  function stateChangeCallback(xhr, xhrProxy) {
-    if (xhr.readyState === 4 && xhr.status !== 0) {
-      handleResponse(xhr, xhrProxy);
-    } else if (xhr.readyState !== 4) {
-      triggerListener(xhr, eventReadyStateChange);
-    }
-    return true;
-  }
-
-  return hook({
-    onload: preventXhrProxyCallback,
-    onloadend: preventXhrProxyCallback,
-    onerror: errorCallback(eventError),
-    ontimeout: errorCallback(eventTimeout),
-    onabort: errorCallback(eventAbort),
-    onreadystatechange: function (xhr) {
-      return stateChangeCallback(xhr, this);
-    },
-    open: function open(args, xhr) {
-      var _this = this;
-      var config = xhr.config = {headers: {}};
-      config.method = args[0];
-      config.url = args[1];
-      config.async = args[2];
-      config.user = args[3];
-      config.password = args[4];
-      config.xhr = xhr;
-      var evName = 'on' + eventReadyStateChange;
-      if (!xhr[evName]) {
-        xhr[evName] = function () {
-          return stateChangeCallback(xhr, _this);
-        };
-      }
-
-      // 如果有请求拦截器，则在调用onRequest后再打开链接。因为onRequest最佳调用时机是在send前，
-      // 所以我们在send拦截函数中再手动调用open，因此返回true阻止xhr.open调用。
-      //
-      // 如果没有请求拦截器，则不用阻断xhr.open调用
-      if (onRequest) return true;
-    },
-    send: function (args, xhr) {
-      var config = xhr.config
-      config.withCredentials = xhr.withCredentials
-      config.body = args[0];
-      if (onRequest) {
-        // In 'onRequest', we may call XHR's event handler, such as `xhr.onload`.
-        // However, XHR's event handler may not be set until xhr.send is called in
-        // the user's code, so we use `setTimeout` to avoid this situation
-        var req = function () {
-          onRequest(config, new RequestHandler(xhr));
-        }
-        config.async === false ? req() : setTimeout(req)
-        return true;
-      }
-    },
-    setRequestHeader: function (args, xhr) {
-      // Collect request headers
-      xhr.config.headers[args[0].toLowerCase()] = args[1];
-      if (onRequest) return true;
-    },
-    addEventListener: function (args, xhr) {
-      var _this = this;
-      if (events.indexOf(args[0]) !== -1) {
-        var handler = args[1];
-        getEventTarget(xhr).addEventListener(args[0], function (e) {
-          var event = configEvent(e, _this);
-          event.type = args[0];
-          event.isTrusted = true;
-          handler.call(_this, event);
-        });
-        return true;
-      }
-    },
-    getAllResponseHeaders: function (_, xhr) {
-      var headers = xhr.resHeader
-      if (headers) {
-        var header = "";
-        for (var key in headers) {
-          header += key + ': ' + headers[key] + '\r\n';
-        }
-        return header;
-      }
-    },
-    getResponseHeader: function (args, xhr) {
-      var headers = xhr.resHeader
-      if (headers) {
-        return headers[(args[0] || '').toLowerCase()];
-      }
-    }
-  }, win);
-}
-
-
-
-
